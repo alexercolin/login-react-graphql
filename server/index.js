dfa44dbf455f2e248a19301f87b519b41072d8de
alexercolin <alex_ercolin@hotmail.com>
2021-10-12 19:47:13 -0300

    feat:creating api with graphql and apollo server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakenin",
+    author: "oi",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
