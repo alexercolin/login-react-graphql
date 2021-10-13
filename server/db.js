54c4506b64775f135c3f2a1fbd119bf278714382
alexercolin <alex_ercolin@hotmail.com>
2021-10-13 09:41:15 -0300

    feat:creating a temporary database

---
+++ server/db.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    id: 1,
+    slug: "alex",
+    username: "alexercolin",
+    password: "alex123",
+    attempts: 0,
+  },
+  {
+    id: 2,
+    slug: "teste",
+    username: "teste",
+    password: "teste123",
+    attempts: 5,
+  },
+  {
+    id: 3,
+    slug: "testando",
+    username: "testando",
+    password: "123teste",
+    attempts: 0,
+  },
+];
+
+module.exports = { users };
