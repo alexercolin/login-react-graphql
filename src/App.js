da868a1624771e117c39960a662acaa59564bbe6
alexercolin <alex_ercolin@hotmail.com>
2021-10-14 09:38:21 -0300

    feat:connect front end with backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import LoginForm from './components/LoginForm'
+import LoginForm from "./components/LoginForm";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <LoginForm />
+  return (
+    <ApolloProvider client={client}>
+      <LoginForm />
+    </ApolloProvider>
+  );
 }
 
 export default App;
