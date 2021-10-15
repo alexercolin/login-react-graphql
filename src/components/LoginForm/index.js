a8ce483ebaebad9df8ef121b1bdc2a3bc1ec7607
alexercolin <alex_ercolin@hotmail.com>
2021-10-15 11:02:21 -0300

    feat: add login method to the front

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,12 +1,12 @@
-import "./style.css";
+import "./styles.scss";
 import { useState } from "react";
-import { FormControl, Box, TextField, Button } from "@material-ui/core";
-import { useQuery,useMutation, gql } from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
 
-const GET_USERS_ATTEMPTS = gql`
-  {
-    users {
-      attempts
+const LOGIN_USER = gql`
+  query ($username: String, $password: String) {
+    login(username: $username, password: $password) {
+      username
+      password
     }
   }
 `;
@@ -23,45 +23,59 @@ const LoginForm = () => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
-  const { data, loading, error } = useQuery(GET_USERS_ATTEMPTS);
-  const [addUser] =useMutation(ADD_NEW_USER)
+  const { data, loading, error } = useQuery(LOGIN_USER, {
+    variables: {
+      username: username,
+      password: password,
+    },
+  });
+  const [addUser] = useMutation(ADD_NEW_USER);
 
-  function handleLogin() {
-    console.log(data, password);
+  const handleLogin = () => {
+    if (data.login === null) {
+      window.alert("Usuário ou senha inválidos!");
+    } else {
+      return window.alert("Login realizado com sucesso!");
+    }
   }
 
   return (
-    <FormControl className="login-form">
-      <Box
-        sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}
-      >
-        <TextField
+    <div className="main__container">
+      <form className="container__login">
+        <h3 className="welcome__message">Welcome, login to your account</h3>
+        <input
+          required
+          placeholder="Username"
+          className="input"
           onChange={(event) => setUsername(event.target.value)}
           id="input-username"
           label="Username"
-          variant="standard"
         />
-        <TextField
+        <input
+          required
+          placeholder="Password"
           onChange={(event) => setPassword(event.target.value)}
           id="input-password"
           label="Password"
           type="password"
-          variant="standard"
         />
-        <Button onClick={handleLogin} variant="contained">
-          Login
-        </Button>
-        <Button onClick={() => {addUser({
-          variables:{
-            username:"alecasso",
-            password:"alecao123"
-          }
-        })}} variant="outlined">
+        <div className="login__buttons">
+        <button onClick={handleLogin}>Login</button>
+        <button
+          onClick={() => {
+            addUser({
+              variables: {
+                username: "alecasso",
+                password: "alecao123",
+              },
+            });
+          }}
+        >
           Sign up
-        </Button>
-        <Button size="small">Forget Password?</Button>
-      </Box>
-    </FormControl>
+        </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
